ed8968e73f3daa8b235d1fcdb3c188b63c1fa6db
laytonco <laytoncozad4@gmail.ccomm>
2025-02-20 15:51:00 -0600

    movement A-D

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import pygame
 from constants import *
+from player import Player
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    x = int(SCREEN_WIDTH / 2) 
+    y = int(SCREEN_HEIGHT / 2)
+    player = Player(x, y)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        def fill(black):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-            pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        #limit FPS to 60    
+        dt = clock.tick(60) / 1000
+
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
 
     
 
