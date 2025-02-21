33eab3b8b961cf4505a40ec614d6a1107a4ae8f7
laytonco <laytoncozad4@gmail.ccomm>
2025-02-21 13:59:54 -0600

    shooting, asteroids, ect

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
-    x = int(SCREEN_WIDTH / 2) 
-    y = int(SCREEN_HEIGHT / 2)
-    player = Player(x, y)
+    
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = updatable
+    astroid_field = AsteroidField()
+    shots_group = pygame.sprite.Group()
+
+
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots_group, updatable, drawable)
+
+    x = int(SCREEN_WIDTH / 2) 
+    y = int(SCREEN_HEIGHT / 2)
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,10 +43,19 @@ def main():
         #limit FPS to 60    
         dt = clock.tick(60) / 1000
 
-        player.update(dt)
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+
+        
 
 
     
